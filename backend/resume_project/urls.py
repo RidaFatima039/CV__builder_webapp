75f7a3f027ec693335ac4e767253884204d5991a
RidaFatima039 <rf5736374@gmail.com>
2026-08-11 14:59:55 +0500

    done push

--- backend/resume_project/urls.py
+++ backend/resume_project/urls.py
@@ -13,5 +13,10 @@ urlpatterns = [
 ]
 
 # Serve media files locally during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
